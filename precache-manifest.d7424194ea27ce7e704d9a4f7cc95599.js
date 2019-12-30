f2998555ffa22b096ad8a8af7a7d1ad28244e10b
Siarhei <sergeylabut@gmail.com>
2019-12-30 12:44:47 +0300

    Updates

---
+++ precache-manifest.d7424194ea27ce7e704d9a4f7cc95599.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "006f32603bf92970b2e29a49cfa66107",
+    "url": "/BlackJackRR/index.html"
+  },
+  {
+    "revision": "8d098d5c9703566c40e4",
+    "url": "/BlackJackRR/static/css/main.d2e546e3.chunk.css"
+  },
+  {
+    "revision": "80658c20c4f18814e60c",
+    "url": "/BlackJackRR/static/js/2.0b4c320b.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/BlackJackRR/static/js/2.0b4c320b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8d098d5c9703566c40e4",
+    "url": "/BlackJackRR/static/js/main.92fa0577.chunk.js"
+  },
+  {
+    "revision": "f3fb4eb9b2140d0bfd3b",
+    "url": "/BlackJackRR/static/js/runtime-main.9f948961.js"
+  },
+  {
+    "revision": "1377219ed307c1690edcc0371d1b908c",
+    "url": "/BlackJackRR/static/media/bender.1377219e.png"
+  }
+]);
